05c9e9395fda475a7e7124c6b2440abb6fc68db7
David Viramontes <dviramontes@gmail.com>
2017-07-17 14:37:39 -0600

    chore: rm internalization assert

--- word-count.spec.js
+++ word-count.spec.js
@@ -49,11 +49,6 @@ describe('count()', () => {
         expect(words.count('Joe can\'t tell between \'large\' and large.')).toEqual(expectedCounts);
     });
 
-    xit('counts properly international characters', () => {
-        const expectedCounts = { 'hola': 1, 'qué': 1, 'tal': 1, 'привет': 1 };
-        expect(words.count('¡Hola! ¿Qué tal? Привет!')).toEqual(expectedCounts);
-    });
-
     xit('counts multiline', () => {
         const expectedCounts = { hello: 1, world: 1 };
         expect(words.count('hello\nworld')).toEqual(expectedCounts);
